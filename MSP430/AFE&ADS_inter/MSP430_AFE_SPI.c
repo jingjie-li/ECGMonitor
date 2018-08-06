7b7b59c9a9233c3d4883b01d61e3a47b2b45886e
lijinmi <lijinmingli@foxmail.com>
2018-08-06 09:56:33 +0800

    ecg

--- MSP430/AFE&ADS_inter/MSP430_AFE_SPI.c
+++ MSP430/AFE&ADS_inter/MSP430_AFE_SPI.c
@@ -184,63 +184,63 @@ void TI_AFE4400_WriteRegSettings(void)
 {
 
   TI_AFE4400_SPIAutoIncWriteReg(LED2STC, 
-                          TI_AFE4400_LED2STC_REG_VALUE, 3);           
+                          LED2STC_VAL, 3);           
   TI_AFE4400_SPIAutoIncWriteReg(LED2ENDC, 
-                          TI_AFE4400_LED2ENDC_REG_VALUE, 3);    
+                          LED2ENDC_VAL, 3);    
   TI_AFE4400_SPIAutoIncWriteReg(LED2LEDSTC, 
-                          TI_AFE4400_LED2LEDSTC_REG_VALUE, 3);  
+                          LED2LEDSTC_VAL, 3);  
   TI_AFE4400_SPIAutoIncWriteReg(LED2LEDENDC, 
-                          TI_AFE4400_LED2LEDENDC_REG_VALUE, 3);   
+                          LED2LEDENDC_VAL, 3);   
   TI_AFE4400_SPIAutoIncWriteReg(ALED2STC, 
-                          TI_AFE4400_ALED2STC_REG_VALUE, 3);           
+                          ALED2STC_VAL, 3);           
   TI_AFE4400_SPIAutoIncWriteReg(ALED2ENDC, 
-                          TI_AFE4400_ALED2ENDC_REG_VALUE, 3);             
+                          ALED2ENDC_VAL, 3);             
   TI_AFE4400_SPIAutoIncWriteReg(LED1STC, 
-                          TI_AFE4400_LED1STC_REG_VALUE, 3);              
+                          LED1STC_VAL, 3);              
   TI_AFE4400_SPIAutoIncWriteReg(LED1ENDC, 
-                          TI_AFE4400_LED1ENDC_REG_VALUE, 3);                
+                          LED1ENDC_VAL, 3);                
   TI_AFE4400_SPIAutoIncWriteReg(LED1LEDSTC, 
-                          TI_AFE4400_LED1LEDSTC_REG_VALUE, 3);            
+                          LED1LEDSTC_VAL, 3);            
   TI_AFE4400_SPIAutoIncWriteReg(LED1LEDENDC, 
-                          TI_AFE4400_LED1LEDENDC_REG_VALUE, 3);              
+                          LED1LEDENDC_VAL, 3);              
   TI_AFE4400_SPIAutoIncWriteReg(ALED1STC, 
-                          TI_AFE4400_ALED1STC_REG_VALUE, 3);             
+                          ALED1STC_VAL, 3);             
   TI_AFE4400_SPIAutoIncWriteReg(ALED1ENDC, 
-                          TI_AFE4400_ALED1ENDC_REG_VALUE, 3);       
+                          ALED1ENDC_VAL, 3);       
   TI_AFE4400_SPIAutoIncWriteReg(LED2CONVST, 
-                          TI_AFE4400_LED2CONVST_REG_VALUE, 3);          
+                          LED2CONVST_VAL, 3);          
   TI_AFE4400_SPIAutoIncWriteReg(LED2CONVEND, 
-                          TI_AFE4400_LED2CONVEND_REG_VALUE, 3);              
+                          LED2CONVEND_VAL, 3);              
   TI_AFE4400_SPIAutoIncWriteReg(ALED2CONVST, 
-                          TI_AFE4400_ALED2CONVST_REG_VALUE, 3);               
+                          ALED2CONVST_VAL, 3);               
   TI_AFE4400_SPIAutoIncWriteReg(ALED2CONVEND, 
-                          TI_AFE4400_ALED2CONVEND_REG_VALUE, 3);                
+                          ALED2CONVEND_VAL, 3);                
   TI_AFE4400_SPIAutoIncWriteReg(LED1CONVST, 
-                          TI_AFE4400_LED1CONVST_REG_VALUE, 3);            
+                          LED1CONVST_VAL, 3);            
   TI_AFE4400_SPIAutoIncWriteReg(LED1CONVEND, 
-                          TI_AFE4400_LED1CONVEND_REG_VALUE, 3);             
+                          LED1CONVEND_VAL, 3);             
   TI_AFE4400_SPIAutoIncWriteReg(ALED1CONVST, 
-                          TI_AFE4400_ALED1CONVST_REG_VALUE, 3);            
+                          ALED1CONVST_VAL, 3);            
   TI_AFE4400_SPIAutoIncWriteReg(ALED1CONVEND, 
-                          TI_AFE4400_ALED1CONVEND_REG_VALUE, 3);   
+                          ALED1CONVEND_VAL, 3);   
   TI_AFE4400_SPIAutoIncWriteReg(ADCRSTSTCT0, 
-                          TI_AFE4400_ADCRSTSTCT0_REG_VALUE, 3);            
+                          ADCRSTSTCT0_VAL, 3);            
   TI_AFE4400_SPIAutoIncWriteReg(ADCRSTENDCT0, 
-                          TI_AFE4400_ADCRSTENDCT0_REG_VALUE, 3);           
+                          ADCRSTENDCT0_VAL, 3);           
   TI_AFE4400_SPIAutoIncWriteReg(ADCRSTSTCT1, 
-                          TI_AFE4400_ADCRSTSTCT1_REG_VALUE, 3);          
+                          ADCRSTSTCT1_VAL, 3);          
   TI_AFE4400_SPIAutoIncWriteReg(ADCRSTENDCT1, 
-                          TI_AFE4400_ADCRSTENDCT1_REG_VALUE, 3);     
+                          ADCRSTENDCT1_VAL, 3);     
   TI_AFE4400_SPIAutoIncWriteReg(ADCRSTSTCT2, 
-                          TI_AFE4400_ADCRSTSTCT2_REG_VALUE, 3);         
+                          ADCRSTSTCT2_VAL, 3);         
   TI_AFE4400_SPIAutoIncWriteReg(ADCRSTENDCT2, 
-                          TI_AFE4400_ADCRSTENDCT2_REG_VALUE, 3);         
+                          ADCRSTENDCT2_VAL, 3);         
   TI_AFE4400_SPIAutoIncWriteReg(ADCRSTSTCT3, 
-                          TI_AFE4400_ADCRSTSTCT3_REG_VALUE, 3);           
+                          ADCRSTSTCT3_VAL, 3);           
   TI_AFE4400_SPIAutoIncWriteReg(ADCRSTENDCT3, 
-                          TI_AFE4400_ADCRSTENDCT3_REG_VALUE, 3);     
+                          ADCRSTENDCT3_VAL, 3);     
   TI_AFE4400_SPIAutoIncWriteReg(PRPCOUNT, 
-                          TI_AFE4400_PRPCOUNT_REG_VALUE, 3);            
+                          PRP, 3);            
   TI_AFE4400_SPIAutoIncWriteReg(CONTROL1, 
                           TI_AFE4400_CONTROL1_REG_VALUE, 3);           
   TI_AFE4400_SPIAutoIncWriteReg(TIA_AMB_GAIN, 
